e3433cfb105f0689caf4333d5d81c69cf367049a
swaso <swaso.awijaya@gmail.com>
2024-08-20 10:24:44 +0700

    implement bcrypt

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,20 @@ const app = express();
 // untuk membaca json, bersifat WAJIB!!!!
 app.use(express.json());
 
-app.post("/register", (req, res) => {
-  console.log(req.body);
-  return res.json({ message: req.body });
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // hash password
+  const hashingPassword = await bcrypt.hash(password, 13);
+
+  // mock data yang akan dimasukkan ke database
+  const newUser = {
+    name,
+    email,
+    password: hashingPassword,
+  };
+
+  return res.json({ message: newUser });
 });
 
 app.listen(process.env.PORT, () => {
